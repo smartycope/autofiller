bc74f937d6b1fe98f916d72ace2cb76c9e45229c
Copeland Carter <smartycope@gmail.com>
2025-07-28 11:39:26 -0500

    Made the config file more generalized. Untested commit!

--- src/autofiller/__main__.py
+++ src/autofiller/__main__.py
@@ -12,26 +12,40 @@ from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
 from qframelesswindow import FramelessWindow
 import random_name
-from ezregex import python as er
+from ezregex import python as ez
 import argparse
+from pathlib import Path
+import json
 
-
+# Parse args
 parser = argparse.ArgumentParser(
-    prog='autofiller',
+    prog='quick-assist',
     description='A small popup to input various random data for you',
 )
 parser.add_argument('-d', '--deamon', action='store_true')
 parser.add_argument('-m', '--modifier', choices=('meta', 'alt', 'ctrl', 'none'), default='meta')
 parser.add_argument('-k', '--key', default='f')
+parser.add_argument('config', type=str, default='~/.config/quick-assist.json', nargs='?')
 
 args = parser.parse_args()
 keyboard = Keyboard()
 mouse = Mouse()
+config = Path(args.config).expanduser()
+
+# Create config if it doesn't exist
+if not config.exists():
+    config.parent.mkdir(parents=True, exist_ok=True)
+    # Copy default config file
+    config.write_text((Path(__file__).parent / 'default_config.json').read_text())
+
+
+# Load config
+config = json.loads(config.read_text())
 
 modifiers = {
     "meta": False,
     "ctrl": False,
-    "alt": False,
+    "alt":  False,
     "none": None,
 }
 
@@ -44,11 +58,12 @@ class Window(FramelessWindow):
         w.setMaximumSize(0, 0)
         self.setTitleBar(w)
 
-        self.setWindowTitle("autofiller")
+        self.setWindowTitle("quick-assist")
         self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
         self.setGeometry(*mouse.position, 100, 100)
 
-        self.body()
+        self.window().setLayout(QVBoxLayout())
+        self.open_menu(config)
 
     def goaway(self):
         if args.deamon:
@@ -66,47 +81,80 @@ class Window(FramelessWindow):
 
         return super().keyPressEvent(event)
 
-    def body(self):
-        layout = QVBoxLayout()
-
+    def open_menu(self, menu_data):
         self.keys = {}
+        # Clear layout
+        while self.layout().count():
+            child = self.layout().takeAt(0)
+            if child.widget():
+                child.widget().deleteLater()
 
-        # To add buttons just add to this, then add to add_random()
-        buttons = [
-            QPushButton("Add Random Text"),
-            QPushButton("Add Random Name"),
-            QPushButton("Add Random JSON"),
-            QPushButton("Add Random Email"),
-        ]
+        for type, data in menu_data.items():
+            # type is the name. The first letter is the key
+            # If data is a dict, it's a submenu
+            if isinstance(data, dict):
+                self.keys[type[0]] = partial(self.open_menu, data)
+            # If data is a list, its a sequence of actions
+            elif isinstance(data, list):
+                self.keys[type[0]] = partial(self.activate_sequence, data)
+            # If data is a string, type it
+            elif isinstance(data, str):
+                self.keys[type[0]] = self.parse_value(data)
 
-        for cnt, button in enumerate(buttons):
-            button.setText(f'{cnt + 1}: ' + button.text())
-            type = button.text().split()[-1].lower()
-            func = partial(self.add_random, type=type)
-            self.keys[(str(cnt+1), type[0])] = func
+            button = QPushButton(type)
+            button.pressed.connect(self.keys[type[0]])
+            self.layout().addWidget(button)
 
-            button.pressed.connect(func)
-            layout.addWidget(button)
+    def parse_value(self, value):
+        """Parse a string from the config, and return a function to execute it"""
+        if value.endswith('()'):
+            return getattr(self, value[:-2])
+        else:
+            return partial(self.type, value)
 
-        self.window().setLayout(layout)
-
-    def give(self, text):
+    def type(self, text):
+        """Type text and close the window"""
         self.goaway()
         sleep(.05)
+        # If it looks like a keyboard sequence, use it
+        # if ez.match_range(1, 2, ez.anyof('ctrl', 'alt', 'meta') + ez.ow) + ez.ow + '+' + ez.ow + ez.anyof(ez.word_char).match(text):
+        #     keyboard.press(key)
+        #     keyboard.release(key)
+        # else:
         keyboard.type(text)
 
-    def add_random(self, type):
-        match type:
-            case 'text':
-                regex = (er.match_range(3, 5, er.word_char) + ' ') * randint(2, 5)
-                self.give(regex.invert()[:-1])
-            case 'name':
-                self.give(random_name.generate_name())
-            case 'json':
-                self.give('{ "TODO": "Random JSON" }')
-            case 'email':
-                regex = er.word + er.opt('-' + er.word) + '@' + er.word + '.' + er.anyof('com', 'net', 'org')
-                self.give(regex.invert())
+    def activate_sequence(self, sequence):
+        """Activate a sequence of actions"""
+        for action in sequence:
+            # If it's a list, it's a mouse move
+            if isinstance(action, list):
+                mouse.position = action
+            # If it's a string, type it, or call a custom method
+            elif isinstance(action, str):
+                self.parse_value(action)()
+
+    # Custom methods
+    def random_name(self):
+        self.type(random_name.generate_name())
+
+    def random_email(self):
+        regex = ez.word + ez.opt('-' + ez.word) + '@' + ez.word + '.' + ez.anyof('com', 'net', 'org')
+        self.type(regex.invert())
+
+    def random_phone(self):
+        self.type('123-456-7890')
+
+    def random_address(self):
+        self.type('123 Main St, Anytown, USA')
+
+    def random_json(self):
+        self.type('{ "TODO": "Random JSON" }')
+
+    def random_text(self):
+        regex = (ez.match_range(3, 5, ez.word_char) + ' ') * randint(2, 5)
+        self.type(regex.invert()[:-1])
+
+
 
 app = QApplication(sys.argv)
 window = Window()
